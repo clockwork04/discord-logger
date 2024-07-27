e2df7621f8e2e86111def62045d398598c78ff10
Snoopie <gemonkies0406@gmail.com>
2024-07-26 22:14:15 -0400

    Added sample secret_example.py

---
+++ config/secrets_example.py
@@ -0,0 +1,5 @@
+#Discord Application Token (https://discord.com/developers/applications)
+TOKEN = 'REPLACE.WITH.TOKEN'
+
+#MongoDB Connection String (https://www.mongodb.com/docs/manual/reference/connection-string/) 
+MONGO_URI = 'mongodb://localhost:27017/'
